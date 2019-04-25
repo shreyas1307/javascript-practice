d2b7417d45eeaf88ebe8ce6ccc755b884ac3e638
shreyas1307 <shreyas.bba@gmail.com>
2019-04-25 18:03:15 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## JavaScript Practice.
+
+# I am trying to improve my JavaScript skills. So I'll be pushing to this repo js files containing practice of methods and other concepts.
--- intro.js
+++ intro.js
@@ -1,6 +1,4 @@
-const welcome = "Hello everyone, this is the first out of many projects I shall be pushing to this repo. I am aiming to get in key practice and improve my knowledge of the language"
-
-
-
+const welcome =
+  "Hello everyone, this is the first out of many projects I shall be pushing to this repo. I am aiming to get in key practice and improve my knowledge of the language";
 
 console.log(welcome);
