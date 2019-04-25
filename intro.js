a5c0018b71192c1f14e57d8a8e8fe59db049ff95
shreyas1307 <shreyas.bba@gmail.com>
2019-04-25 18:01:52 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,6 @@
+const welcome = "Hello everyone, this is the first out of many projects I shall be pushing to this repo. I am aiming to get in key practice and improve my knowledge of the language"
+
+
+
+
+console.log(welcome);
