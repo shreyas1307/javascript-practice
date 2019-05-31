f84f5cc959c4597acac0567bfa19a53e181ef9fe
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-05-31 07:30:08 +0530

    misc

--- JavaScript - Understanding Weird Parts/app.js
+++ JavaScript - Understanding Weird Parts/app.js
@@ -26,3 +26,23 @@ function a() {
 
 var myVar = 1;
 a();
+
+var person = new Object();
+
+person["firstname"] = "Tony";
+person["lastname"] = "Alicea";
+
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]);
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+person.address = new Object();
+person.address.street = "111 Main St.";
+person.address.city = "New York";
+person.address.state = "NY";
+
+console.log(person);
